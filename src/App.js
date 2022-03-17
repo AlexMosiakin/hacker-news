86c3bef6e71749d1ff33c9c5717fd8dadca3309b
alex_mosiakin <mosyakin.a@flat12.ru>
2022-03-17 20:28:14 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route exact path='/news' element={<News />}/>
-        <Route exact path='/news/:id' element={<NewItem />}/>
-        <Route path="*" element={<Navigate to="/news" />}/>
+        <Route exact path='/hacker-news//news' element={<News />}/>
+        <Route exact path='/hacker-news/news/:id' element={<NewItem />}/>
+        <Route path="*" element={<Navigate to="/hacker-news/news" />}/>
       </Routes>
     </BrowserRouter>
   );
